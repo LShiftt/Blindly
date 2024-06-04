4aa66e0c24d96dd2155d66279e2580dc6a7ef30e
Mathis Dubois <duboismathis001@gmail.com>
2024-06-04 15:16:09 +0200

    UPd

--- helpers/functions.php
+++ helpers/functions.php
@@ -16,14 +16,12 @@ function head(string $title = ''): string
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
   <style>
   </style>
 
   <title>$title</title>
 </head>
 <body>
-<script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
 <header>
 </header>
 HTML_HEAD;
--- index.php
+++ index.php
@@ -8,7 +8,7 @@ echo head("home");
   <hr class="my-4">
   <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
   <p class="lead">
-    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+    <a class="btn btn-primary btn-lg" href="./pages/musiswipe.php" role="button">Musiswipe</a>
   </p>
 </div>
 
