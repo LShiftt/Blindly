eb8e1f91aec231ec9c3f9b0913ed478bc2a794f8
Mathis Dubois <duboismathis001@gmail.com>
2024-06-05 10:17:19 +0200

    upd

--- pages/musiswipe.php
+++ pages/musiswipe.php
@@ -13,21 +13,59 @@ $sql = 'SELECT * FROM `song` WHERE 1 ORDER BY id ASC'; // by random
     $songs = $sth->fetchAll();
 ?>
 
+ <div class="test-element">Element</div>
+    <script>function startDrag(e) {
+
+            this.ontouchmove = this.onmspointermove = moveDrag;
+
+            this.ontouchend = this.onmspointerup = function () {
+                this.ontouchmove = this.onmspointermove = null;
+                this.ontouchend = this.onmspointerup = null;
+            }
+
+            var pos = [this.offsetLeft, this.offsetTop];
+            var that = this;
+            var origin = getCoors(e);
+
+            function moveDrag(e) {
+                var currentPos = getCoors(e);
+                var deltaX = currentPos[0] - origin[0];
+                var deltaY = currentPos[1] - origin[1];
+                this.style.left = (pos[0] + deltaX) + 'px';
+                this.style.top = (pos[1] + deltaY) + 'px';
+                return false; // cancels scrolling
+            }
+
+            function getCoors(e) {
+                var coors = [];
+                if (e.targetTouches && e.targetTouches.length) {
+                    var thisTouch = e.targetTouches[0];
+                    coors[0] = thisTouch.clientX;
+                    coors[1] = thisTouch.clientY;
+                } else {
+                    coors[0] = e.clientX;
+                    coors[1] = e.clientY;
+                }
+                return coors;
+            }
+        }
+
+        var elements = document.querySelectorAll('.test-element');
+        [].forEach.call(elements, function (element) {
+            element.ontouchstart = element.onmspointerdown = startDrag;
+        });
+
+        document.ongesturechange = function () {
+            return false;
+        }
+    </script>
 <?php
 
-dump($songs);
+// dump($songs);
 
 
 foreach ($songs as $song) {
-  
-  echo '
-  <article>
-  <h1>'.$song["genre"].'</h1>
-  <h2>'.$song["title"].', par : <i>'.$song["author"].'</i></h2>
-  <img src="'.$song["image"].'">
-  <audio controls src="'.$song["url"].'" ></audio>
-  <p>'.$song["url"].'</p>
-</article>';
+
 };
 ?>
 <?= foot();?>
