6cf0d36b92aa7cf247794a252054ec07e0b4a53f
LShiftt <Lshiftprog@gmail.com>
2024-06-12 15:48:17 +0200

    upd luminosity

---
+++ assets/js/luminosity.js
@@ -0,0 +1,31 @@
+window.addEventListener('DOMContentLoaded', function(){
+
+    var toggleDarkLight = document.querySelector(".fa-sun");
+ 
+     var storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+     if (storedTheme)
+         document.documentElement.setAttribute('data-theme', storedTheme)
+     
+     
+     toggleDarkLight.onclick = function() {
+         var currentTheme = document.documentElement.getAttribute("data-theme");
+         var targetTheme = "light";
+     
+         if (currentTheme === "light") {
+             targetTheme = "dark";
+             toggleDarkLight.classList.add("dark");
+             toggleDarkLight.classList.remove("light");
+ 
+         }else{
+             targetTheme = "light";
+             toggleDarkLight.classList.add("light");
+             toggleDarkLight.classList.remove("dark");
+ 
+         }
+     
+         document.documentElement.setAttribute('data-theme', targetTheme)
+         localStorage.setItem('theme', targetTheme);
+     };
+     
+     
+ });
